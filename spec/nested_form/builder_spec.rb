17b1a7dfa7832a42f5b83a08c7a927cb23a8c93b
Michel Benevento <michel@air.local>
2011-03-15 13:17:26 +0100

    test associations with optional arguments

--- spec/nested_form/builder_spec.rb
+++ spec/nested_form/builder_spec.rb
@@ -13,6 +13,10 @@ describe NestedForm::Builder do
       @builder.link_to_add("Add", :tasks).should == '<a href="javascript:void(0)" class="add_nested_fields" data-association="tasks">Add</a>'
     end
 
+    it "should have an add link with arguments" do
+      @builder.link_to_add("Add", :tasks, :foo => 'bar').should == '<a href="javascript:void(0)" class="add_nested_fields" data-association="tasks_with_foo_bar">Add</a>'
+    end
+    
     it "should have a remove link" do
       @builder.link_to_remove("Remove").should == '<input id="item__destroy" name="item[_destroy]" type="hidden" value="false" /><a href="javascript:void(0)" class="remove_nested_fields">Remove</a>'
     end
